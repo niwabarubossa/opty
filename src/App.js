fadd708320076a34c7e989bf99c566f90c3aacf4
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-12 19:01:34 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import TweetsContainer from './components/TweetsContainer'
 import Header from './components/Header'
 import MainContainer from './components/MainContainer'
-
 import GetPostListContainer from './components/containers/GetPostListContainer'
 import { getPostsRequest, getPostsSuccess, getPosts } from './actions'
 import { connect } from 'react-redux'
@@ -13,7 +11,7 @@ import { connect } from 'react-redux'
 class App extends Component {
   componentDidMount(){
     this.props.getPosts()
-}
+  }
   render() {
     return (
       <div className="App">
@@ -26,15 +24,9 @@ class App extends Component {
 }
 
 const mapDispatchToProps = ({ getPostsRequest, getPostsSuccess, getPosts })
-// const mapStateToProps = (state, ownProps) => {
-//   const event = 'aaa'
-//   return { initialValues: event, event }
-// }
 const mapStateToProps = (state) => {    
-  // const count = state.posts.count
   const length = Object.keys(state.firebase).length
   const currentState = state.firebase[length-1].items
   return { state_posts: currentState }
 }
-// export default App;
 export default connect(mapStateToProps, mapDispatchToProps)(App)
