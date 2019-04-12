7dd448c2142a4930d041ee4a362ba2a584e33a9d
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-12 18:36:07 +0900

    ok?

--- src/components/TestComponentChild.js
+++ src/components/TestComponentChild.js
@@ -7,7 +7,7 @@ const TestComponentChild = ({props}) => {
       { props.state_posts && props.state_posts.map(project => {
         return (
             <div>
-              <h1>test component child </h1>
+              <h1 style={{color: 'black'}}>test component child </h1>
               <h1 style={{color: 'black'}}>{project.content}</h1>
             </div>
         )
