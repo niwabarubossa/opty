07b842fdc5e1543d3095be504a946b3e0cb87941
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-13 17:26:32 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react';
-import classes from './App.css';
 import MainContainer from './components/MainContainer'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import TestComponent from './components/TestComponent';
-// import AppBarDrawer from './components/appbar/AppBarDrawer'
 import AppBarMain from './components/appbar/AppBarMain'
 
-const drawerWidth = 240;
-
-const content_style = {
-  flexGrow: 1,
-  padding: '5px',
-  paddingTop: '64px'
-}
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+});
 
 class App extends Component {
 
   render() {
-    // const { classes, theme } = this.props;
+    const { classes, theme } = this.props;
     return (
       <BrowserRouter>
         <Switch>
-            {/* <div className="App"> */}
-            <div className={classes.App}>
+            <div className="App" style={{textAlign: 'center'}}>
               <div className={classes.root}>
                 <AppBarMain />
                   
-                <main className={classes.content} style={content_style} >
+                <main className={classes.content} >
                 <BrowserRouter>
                   <Switch>
                     <Route exact path="/" component={MainContainer} />
@@ -48,5 +47,4 @@ App.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired,
 };
-// export default withStyles(styles, { withTheme: true })(App);
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
