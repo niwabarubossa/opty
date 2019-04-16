0b867701ff2093645c1926fd0d7837864b52f842
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-16 21:01:16 +0900

    appbar main .js

--- src/components/appbar/AppBarMain.js
+++ src/components/appbar/AppBarMain.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { handleDrawerToggle } from '../../actions'
-import AppBarDrawer from './AppBarDrawer'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -17,15 +14,6 @@ import Typography from '@material-ui/core/Typography';
 
 const drawerWidth = 240;
 const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
   appBar: {
     marginLeft: drawerWidth,
     [theme.breakpoints.up('sm')]: {
@@ -37,35 +25,10 @@ const styles = theme => ({
     [theme.breakpoints.up('sm')]: {
       display: 'none',
     },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-  },
+  }
 });
 
 class AppBarMain extends Component {
-    constructor() {
-        super();
-        this.state = {
-          showPopup: false
-        };
-    }
-    // state = {
-    //     mobileOpen: false,
-    // };
-    handleDrawerToggle = () => {
-        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-    };
-    togglePopup() {
-      this.setState({
-        showPopup: !this.state.showPopup
-      });
-    }
 
     render(){
         const { classes, theme } = this.props;
@@ -86,7 +49,6 @@ class AppBarMain extends Component {
                     <Typography variant="h6" color="inherit" noWrap>
                         <Link to={'/'} style={{textDecoration : 'none',color: 'white' }}>Opty</Link>
                         <Link to={'/management'} style={{textDecoration : 'none',color: 'white',marginLeft: '10px' }}>管理画面へ</Link>
-                        {/* <button onClick={this.togglePopup.bind(this)}>記録する</button> */}
                     </Typography>
                   </Toolbar>
                 </AppBar>
@@ -99,12 +61,9 @@ AppBarMain.propTypes = {
     classes: PropTypes.object.isRequired,
     theme: PropTypes.object.isRequired,
 };
-// export default withStyles(styles, { withTheme: true })(AppBarMain);
 
 const mapDispatchToProps = ({ handleDrawerToggle })
 const mapStateToProps = (state) => {    
-  // const length = Object.keys(state.firebase).length
-  // const current_mobile_open = state.firebase[length-1].mobileOpen
   return { redux_mobileOpen: state.firebase.mobileOpen }
 }
 
