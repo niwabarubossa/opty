376762c5317d20e3174b256739e4d5f85403e491
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-16 21:11:01 +0900

    postlist.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,24 +3,15 @@ import Post from './Post'
 
 class PostList extends Component{
   render() {
-    console.log('i am post list in state_posts')
-    console.log(this.props.state_posts);
     return(
       <div>
-      
-      { this.props.state_posts && this.props.state_posts.map((state_post) => 
-        <Post key={state_post.content} {...state_post} />
-      )}
-aaa
+        { this.props.state_posts && this.props.state_posts.map((state_post) => 
+          <Post key={state_post.content} {...state_post} />
+        )}
+        aaa
       </div>
     )
     
   }
 }
-
-var hairetu = [{title: 'aiueo',body: 'aiueo'},{title: 'kaki',body: 'bady'}]
-hairetu.map((one) =>
-    console.log(one.body)
-)
-var hairetu = [{title: 'aiueo',body: 'aiueo'},{title: 'kaki',body: 'bady'}]
 export default PostList
